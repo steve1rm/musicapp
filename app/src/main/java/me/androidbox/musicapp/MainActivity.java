7f3406dd7fcbbc1b815eefe78cf2935b1245bca6
Steve Mason <steve@androidbox.me>
2016-07-14 23:27:29 +0700

    Added toolbar and tablayout

--- app/src/main/java/me/androidbox/musicapp/MainActivity.java
+++ app/src/main/java/me/androidbox/musicapp/MainActivity.java
@@ -1,5 +1,6 @@
 package me.androidbox.musicapp;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, MusicAppView.getNewInstance(), "MusicApp");
+            fragmentTransaction.commit();
+        }
     }
 }
--- app/src/main/java/me/androidbox/musicapp/MusicAppView.java
+++ app/src/main/java/me/androidbox/musicapp/MusicAppView.java
@@ -2,8 +2,13 @@ package me.androidbox.musicapp;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,9 +26,32 @@ public class MusicAppView extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.music_app_view, container, false);
+        final View view = inflater.inflate(R.layout.music_app_view, container, false);
+
+        final Toolbar toolbar = (Toolbar)view.findViewById(R.id.tool_bar);
+        AppCompatActivity activity = (AppCompatActivity)getActivity();
+        activity.setSupportActionBar(toolbar);
+
+
+        TabLayout tabLayout = (TabLayout)view.findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_popular));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_nearby));
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main, menu);
     }
 
 }
